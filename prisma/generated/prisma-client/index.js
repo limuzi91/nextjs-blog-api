53ec31292ccd8cde68167f6f33e2f892fd2672f9
Michael <paintermichael.lee@gmail.com>
2020-04-29 16:09:08 -0400

    added env PRISMA_ENDPOINT

--- prisma/generated/prisma-client/index.js
+++ prisma/generated/prisma-client/index.js
@@ -6,16 +6,16 @@ var typeDefs = require("./prisma-schema").typeDefs;
 var models = [
   {
     name: "User",
-    embedded: false
+    embedded: false,
   },
   {
     name: "Post",
-    embedded: false
-  }
+    embedded: false,
+  },
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://blog-api-cbca452880.herokuapp.com/blog-api-service/dev`
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`,
 });
 exports.prisma = new exports.Prisma();
