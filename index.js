e9a57c07b1b7889c386f824550f2b34f6fe8593b
Michael <paintermichael.lee@gmail.com>
2020-04-29 14:46:27 -0400

    rest api done

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const { prisma } = require("./generated/prisma-client");
+const { prisma } = require("./prisma/generated/prisma-client");
 
-// A `main` function so that we can use async/await
-async function main() {
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: "Alice" });
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+const express = require("express");
+const bodyParser = require("body-parser");
 
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users();
-  console.log(allUsers);
-}
+const app = express();
 
-main().catch((e) => console.error(e));
+app.use(bodyParser.json());
+
+app.get(`/posts/published`, async (req, res) => {
+  const publishedPosts = await prisma.posts({ where: { published: true } });
+  res.json(publishedPosts);
+});
+
+app.get("/post/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const post = await prisma.post({ id: postId });
+  res.json(post);
+});
+
+app.get("/posts/user/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const postsByUser = await prisma.user({ id: userId }).posts();
+  res.json(postsByUser);
+});
+
+app.post("/user", async (req, res) => {
+  const newUser = await prisma.createUser(req.body);
+  res.json(newUser);
+});
+
+app.post("/post/draft", async (req, res) => {
+  const newPost = await prisma.createPost(req.body);
+  res.json(newPost);
+});
+
+app.put(`/post/publish/:postId`, async (req, res) => {
+  const { postId } = req.params;
+  const updatedPost = await prisma.updatePost({
+    where: { id: postId },
+    data: { published: true },
+  });
+  res.json(updatedPost);
+});
+
+app.listen(3000, () =>
+  console.log("Server is running on http://localhost:3000")
+);
